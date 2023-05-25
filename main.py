e9ddf821ff0a8adaf00bd8304003bbbec330edea
Jt <mslwp@outlook.com>
2023-05-25 09:27:06 +0800

    Add merge boundaries method.

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def set_axs_properties(axs):
     axs.plot(1, 0, ">k", transform=axs.get_yaxis_transform(), clip_on=False)
     axs.plot(0, 1, "^k", transform=axs.get_xaxis_transform(), clip_on=False)
     
-    axs.axis([-5, 70, -10, 10])
+    axs.axis([0, 70, -10, 10])
     axs.set_aspect('equal', adjustable='datalim')
     axs.grid(ls='--')
     axs.set_xlabel('Longitude/m')  # Add an x-label to the axes.
@@ -107,7 +107,7 @@ def draw_boundary(frame, boundaries, count, axs):
     return tuple(lines)
 
 def anim_history(store_num):
-    fig, axs = plt.subplots(figsize=(20, 5), layout='constrained')  # a figure with a 2x2 grid of Axes
+    fig, axs = plt.subplots(figsize=(70, 10), layout='constrained')  # a figure with a 2x2 grid of Axes
     bnds = [Cboundary(-1.761356, -0.004106, -0.000393, -0.000012, 0, 52.81),
             Cboundary(-2.779935, -0.000795, -0.000415, -0.000012, 0, 40.83),
             Cboundary(-3.787721, -0.005490, -0.000436, -0.000012, 0, 30.64),
@@ -123,6 +123,25 @@ def anim_history(store_num):
 def transform(boundary_orig):
     return boundary_orig
 
+def ref_function(x):
+    shift_position = 20
+    scale = 0.1
+    y = 1 / (1 + scale * math.exp(-1 * (x - shift_position)))
+    return y
+
+def merge_polynomial(former_boundary, later_boundary, ref_function):
+    x_list = np.linspace(0, 60, 25)
+    count = len(x_list)
+    coeff = [ref_function(x) for x in x_list]
+    former_latitude = former_boundary.calculateY_list(x_list)
+    later_latitude = later_boundary.calculateY_list(x_list)
+    merge_latitude = [(1-coeff[index])*former_latitude[index] + coeff[index]*later_latitude[index] for index in list(range(count))]
+    polynomial_function = np.polyfit(x_list, merge_latitude, 3)
+    polynomial3 = np.poly1d(polynomial_function)
+    boundary_merged = Cboundary(polynomial_function[3], polynomial_function[2],polynomial_function[1],polynomial_function[0], 0, 60)
+    print('Fitting polynomial function coefficient(x^3, x^2, x^1, x^0): ', polynomial_function)
+    return boundary_merged
+
 class callback_manager:
     def __init__(self, fig, ax, data_list, history_cache_number=3):
         self.fig = fig
@@ -131,11 +150,16 @@ class callback_manager:
         self.index_position = -1
         self.history_cache_number = history_cache_number
         self.history_cache = []
+        self.merged_cache = []
         
     def clear_all_history(self):
         for history_line in self.history_cache:
             history_line.remove()
         self.history_cache = []
+
+        for history_line in self.merged_cache:
+            history_line.remove()
+        self.merged_cache = []
 
     def on_press(self, event):
         print('press', event.key)
@@ -161,14 +185,29 @@ class callback_manager:
         #     self.exist_history.remove()
         self.clear_all_history()
 
+        # for index in list(range(self.history_cache_number)):
+        #     index_cropped = min(list_len - 1, max(0, self.index_position - index))
+        #     data = self.data_list[index_cropped]
+        #     boundary = data[1]
+        #     boundary = transform(boundary)
+        #     line_drawed = boundary.draw_polynomial(self.ax, boundarycolor=(1.,0.,0.,1.0 / (2*index+1)))
+        #     self.history_cache.append(line_drawed)
+
         for index in list(range(self.history_cache_number)):
             index_cropped = min(list_len - 1, max(0, self.index_position - index))
             data = self.data_list[index_cropped]
             boundary = data[1]
             boundary = transform(boundary)
-            line_drawed = boundary.draw_polynomial(self.ax, boundarycolor=(1.,0.,0.,1.0 / (2*index_cropped+1)))
-            self.history_cache.append(line_drawed)
+            index_former_cropped = min(list_len - 1, max(0, self.index_position - index - 1))
+            if index_former_cropped < index_cropped:
+                data_former = self.data_list[index_former_cropped]
+                boundary_former = data_former[1]
+                boundary_former = transform(boundary_former)
+                boundary_merged = merge_polynomial(boundary_former, boundary, ref_function)
+                line_drawed_merged = boundary_merged.draw_polynomial(self.ax, boundarycolor=(0.,0.,1.,1.0 / (2*index+1)))
+                self.merged_cache.append(line_drawed_merged)
 
+            
         current_index = min(list_len - 1, max(0, self.index_position))
         image_number = self.data_list[current_index][0]
         self.ax.set_title(image_number)
@@ -178,15 +217,10 @@ class callback_manager:
         #     visible = xl.get_visible()
         #     xl.set_visible(not visible)
         #     fig.canvas.draw()
-        
+
 def function_keypress():
     fig, ax = plt.subplots(figsize=(20, 5), layout='constrained')
     data_list = parse_csv('test.csv')
-
-    bnds = [Cboundary(-1.761356, -0.004106, -0.000393, -0.000012, 0, 52.81),
-            Cboundary(-2.779935, -0.000795, -0.000415, -0.000012, 0, 40.83),
-            Cboundary(-3.787721, -0.005490, -0.000436, -0.000012, 0, 30.64)
-            ]
     
     set_axs_properties(ax)
     callback_obj = callback_manager(fig, ax, data_list) 
@@ -235,7 +269,7 @@ def parse_csv(file_path):
 
     return data_list
 
-def radom_sample():
+def random_sample():
     np.random.seed(19680801)  # seed the random number generator.
     data = {'a': np.arange(50),
             'c': np.random.randint(0, 50, 50),
